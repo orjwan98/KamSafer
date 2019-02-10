c3322b042bf2af8fa0b406dd56860018815a146e
samaamro20 <samaamro20@gmail.com>
2019-02-10 11:17:42 +0200

    basic configration #17

--- src/server.js
+++ src/server.js
@@ -1,8 +1,5 @@
 const app = require('./app.js');
-// const { seq } = require('../database/models/index.js');
 
-// seq.then(() => {
 app.listen(app.get('port'), () => {
                 console.log('Our app running  on port', app.get('port')); //eslint-disable-line
 });
-// }).catch(err => console.log('err', err));     //eslint-disable-line
