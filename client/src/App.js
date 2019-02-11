131602953ad538303f26adea8526e662fc48c2a4
orjwan98 <orjwan98@hotmail.com>
2019-02-11 15:54:21 +0200

    added KamSafer's name and modified the route

--- client/src/App.js
+++ client/src/App.js
@@ -9,14 +9,16 @@ import Confirm from "./components/Confirm";
 import Show from "./components/Show";
 import Calendar from "./components/Calendar";
 import { BrowserRouter, Route } from "react-router-dom";
+import "typeface-roboto";
+
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <React.Fragment>
+          <Route path="/" component={Header} />
           <Route exact path="/cars" component={Cars} />
           <Route exact path="/" component={Login} />
-          <Route exact path="/" component={Header} />
           <Route exact path="/" component={Home} />
           <Route exact path="/" component={Footer} />
           <Route exact path="/" component={Add} />
--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,14 +1,16 @@
 import React from "react";
 import KS_logo from "./KS_logo.png";
 import Exit from "./Exit.png";
+import Typography from "@material-ui/core/Typography";
 const Header = () => {
   return (
     <div className="header">
       <div>
-        <img className="logo" alt="logo" src={KS_logo} />
+        <img className="exit" alt="logo" src={Exit} />
       </div>
       <div>
-        <img className="exit" alt="logo" src={Exit} />
+        <img className="logo" alt="logo" src={KS_logo} />
+        <h1>KamSafer</h1>
       </div>
     </div>
   );
