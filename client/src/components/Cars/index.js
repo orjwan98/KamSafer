72adf858a69db447c1a599a4e8a6ae4bc2fb8685
orjwan98 <orjwan98@hotmail.com>
2019-02-11 15:56:08 +0200

    use iteration instead of manually adding data

--- client/src/components/Cars/index.js
+++ client/src/components/Cars/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import Car from "../Car";
 import data from "../../data.json";
+
 class Cars extends Component {
   constructor(props) {
     super(props);
