457f18eba2aee7a030ed4331123f372120e338df
orjwan98 <orjwan98@hotmail.com>
2019-02-11 13:29:42 +0200

    setup Car component and its props

--- client/src/components/Car/index.js
+++ client/src/components/Car/index.js
@@ -1,8 +1,10 @@
 import React from "react";
-const Car = () => {
+const Car = ({ owner, model, car_no }) => {
   return (
     <div className="car">
-      <h1> Hello World Car!</h1>
+      <h2>{owner}'s Car</h2>
+      <h4>{model}</h4>
+      <h4>{car_no}</h4>
     </div>
   );
 };
