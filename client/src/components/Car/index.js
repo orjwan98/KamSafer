5b3a1651d700460cf4042d6515495d7c399d630e
orjwan98 <orjwan98@hotmail.com>
2019-02-11 15:57:02 +0200

    use material ui basic typography and card component

--- client/src/components/Car/index.js
+++ client/src/components/Car/index.js
@@ -1,11 +1,17 @@
 import React from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from "@material-ui/core/CardContent";
+import Typography from "@material-ui/core/Typography";
+
 const Car = ({ owner, model, car_no }) => {
   return (
-    <div className="car">
-      <h2>{owner}'s Car</h2>
-      <h4>{model}</h4>
-      <h4>{car_no}</h4>
-    </div>
+    <Card className="car">
+      <CardContent>
+        <Typography variant="h6">{owner}'s Car</Typography>
+        <Typography variant="subtitle1">{model}</Typography>
+        <Typography variant="subtitle1">{car_no}</Typography>
+      </CardContent>
+    </Card>
   );
 };
 export default Car;
