9fb98aeaf8f543d25d9e6ef9f988656195c332d9
orjwan98 <orjwan98@hotmail.com>
2019-02-11 13:28:17 +0200

    download react router and set up /cars route

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Login from "./components/Login";
 import Header from "./components/Header";
-import Car from "./components/Car";
 import Cars from "./components/Cars";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
@@ -9,22 +8,23 @@ import Add from "./components/Add";
 import Confirm from "./components/Confirm";
 import Show from "./components/Show";
 import Calendar from "./components/Calendar";
-
+import { BrowserRouter, Route } from "react-router-dom";
 class App extends Component {
   render() {
     return (
-      <React.Fragment>
-        <Login />
-        <Header />
-        <Car />
-        <Cars />
-        <Home />
-        <Footer />
-        <Add />
-        <Confirm />
-        <Show />
-        <Calendar />
-      </React.Fragment>
+      <BrowserRouter>
+        <React.Fragment>
+          <Route exact path="/cars" component={Cars} />
+          <Route exact path="/" component={Login} />
+          <Route exact path="/" component={Header} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/" component={Footer} />
+          <Route exact path="/" component={Add} />
+          <Route exact path="/" component={Confirm} />
+          <Route exact path="/" component={Show} />
+          <Route exact path="/" component={Calendar} />
+        </React.Fragment>
+      </BrowserRouter>
     );
   }
 }
