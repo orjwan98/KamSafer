8027d34abe0458fb15839d6f07aaef34da9845b5
orjwan98 <orjwan98@hotmail.com>
2019-02-12 10:58:33 +0200

    modify header's icons

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,12 +1,13 @@
 import React from "react";
 import KS_logo from "./KS_logo.png";
-import Exit from "./Exit.png";
+import Logout from "./Logout.png";
+import Replay from "./Reply.png";
 import Typography from "@material-ui/core/Typography";
 const Header = () => {
   return (
     <div className="header">
       <div>
-        <img className="exit" alt="logo" src={Exit} />
+        <img className="exit" alt="logo" src={Logout} />
       </div>
       <div>
         <img className="logo" alt="logo" src={KS_logo} />
