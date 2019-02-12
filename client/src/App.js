c9e6d915e5fea40dac9cff9793a2d5a1fdaf32c7
orjwan98 <orjwan98@hotmail.com>
2019-02-12 15:55:11 +0200

    add /reports

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,9 @@ import Header from "./components/Header";
 import Cars from "./components/Cars";
 import Home from "./components/Home";
 import Footer from "./components/Footer";
-import Add from "./components/Add";
 import Confirm from "./components/Confirm";
-import Show from "./components/Show";
 import Calendar from "./components/Calendar";
+import Reports from "./components/Reports";
 import { BrowserRouter, Route } from "react-router-dom";
 import data from "./data.json";
 import "typeface-roboto";
@@ -34,12 +33,11 @@ class App extends Component {
             path="/cars"
             component={() => <Cars data={data} handler={this.selectCar} />}
           />
+          <Route exact path="/reports" component={Reports} />
           <Route exact path="/" component={Login} />
           <Route exact path="/" component={Home} />
           <Route exact path="/" component={Footer} />
-          <Route exact path="/" component={Add} />
           <Route exact path="/" component={Confirm} />
-          <Route exact path="/" component={Show} />
           <Route exact path="/" component={Calendar} />
         </React.Fragment>
       </BrowserRouter>
