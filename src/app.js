bf545ab525a528454429c088a2e5c18697638afe
orjwan98 <orjwan98@hotmail.com>
2019-02-13 11:51:42 +0200

    add bodyparser and urlencoded

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
 const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
+const router = require('./controllers');
 
 const app = express();
-app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(router);
 
-app.set('port', 4000);
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+app.set('port', 4556);
 module.exports = app;
