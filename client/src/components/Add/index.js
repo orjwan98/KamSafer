e39e10f715a3ffa6c78f6478427d0a87c9105af1
orjwan98 <orjwan98@hotmail.com>
2019-02-14 16:00:08 +0200

    add material UI styles and add component handlers

--- client/src/components/Add/index.js
+++ client/src/components/Add/index.js
@@ -1,109 +1,85 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import Radio from '@material-ui/core/Radio';
-import RadioGroup from '@material-ui/core/RadioGroup';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormControl from '@material-ui/core/FormControl';
-import FormLabel from '@material-ui/core/FormLabel';
-import TextField from '@material-ui/core/TextField';
-import Input from '@material-ui/core/Input';
-import InputLabel from '@material-ui/core/InputLabel';
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormControl from "@material-ui/core/FormControl";
+import FormLabel from "@material-ui/core/FormLabel";
+import TextField from "@material-ui/core/TextField";
+import Input from "@material-ui/core/Input";
+import InputLabel from "@material-ui/core/InputLabel";
+import green from "@material-ui/core/colors/green";
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
+    color: green[600],
+    "&$checked": {
+      color: green[500]
+    }
   },
   formControl: {
     margin: theme.spacing.unit * 3,
-    direction:'row',
+    direction: "row"
   },
   group: {
     margin: `${theme.spacing.unit}px 0`,
+    flexDirection: "row"
   },
-  Purpose:{
-    float:'left',
-    direction:'row',
+  startEnd: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    margin: "10px"
+  },
+
+  Purpose: {
+    float: "left",
+    direction: "row"
   },
   margin: {
-    margin: theme.spacing.unit,
+    margin: theme.spacing.unit
   },
-  cssLabel: {
-    '&$cssFocused': {
-    },
+  narrow: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 100
   },
-  cssFocused: {},
-  cssUnderline: {
-    '&:after': {
-    },
-  },
-  cssOutlinedInput: {
-    '&$cssFocused $notchedOutline': {
-    },
-  },
-  notchedOutline: {},
-  bootstrapRoot: {
-    'label + &': {
-      marginTop: theme.spacing.unit * 3,
-    },
-  },
-  bootstrapInput: {
-    borderRadius: 4,
-    position: 'relative',
-    backgroundColor: theme.palette.common.white,
-    border: '1px solid #ced4da',
-    fontSize: 16,
-    width: 'auto',
-    padding: '10px 12px',
-    transition: theme.transitions.create(['border-color', 'box-shadow']),
-    // Use the system font instead of the default Roboto font.
-    fontFamily: [
-      '-apple-system',
-      'BlinkMacSystemFont',
-      '"Segoe UI"',
-      'Roboto',
-      '"Helvetica Neue"',
-      'Arial',
-      'sans-serif',
-      '"Apple Color Emoji"',
-      '"Segoe UI Emoji"',
-      '"Segoe UI Symbol"',
-    ].join(','),
-    '&:focus': {
-      borderRadius: 4,
-      borderColor: '#80bdff',
-      boxShadow: '0 0 0 0.2rem rgba(0,123,255,.25)',
-    },
-  },
-  bootstrapFormLabel: {
-    fontSize: 18,
-  },
+  normal: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    marginBottom: theme.spacing.unit
+  }
 });
-
 
 class RadioButtonsGroup extends React.Component {
   state = {
-    value: 'personal',
+    value: "personal"
   };
 
-  handleChange = event => {
-    this.setState({ value: event.target.value });
-  };
   render() {
     const { classes } = this.props;
     return (
       <div className={classes.root}>
-        <FormControl flexDirection="row" component="fieldset" className={classes.formControl}>
-        <FormLabel component="legend" className='Purpose'>Purpose</FormLabel>
+        <FormControl
+          flexDirection="row"
+          component="fieldset"
+          className={classes.formControl}
+        >
+          <FormLabel component="legend" className="Purpose">
+            Purpose
+          </FormLabel>
           <RadioGroup
             aria-label="Purpose"
-            name="Purpose"
+            name="purpose"
             className={classes.group}
             value={this.state.value}
-            onChange={this.handleChange}>
+          >
             <FormControlLabel
               value="Buisness"
-              control={<Radio color="primary" />}
+              control={
+                <Radio color="primary" onChange={this.props.handleChange} />
+              }
               label="Buisness"
               labelPlacement="start"
             />
@@ -114,82 +90,62 @@ class RadioButtonsGroup extends React.Component {
               labelPlacement="start"
             />
           </RadioGroup>
-          <TextField id="text" type="text" value="Name"/>
-          <InputLabel
-          htmlFor="custom-css-standard-input"
-          classes={{
-            root: classes.cssLabel,
-            focused: classes.cssFocused,
-          }}
-        >
-          StartKM
-        </InputLabel>
-        <Input
-          id="custom-css-standard-input"
-          classes={{
-            underline: classes.cssUnderline,
-          }}
-        />
-        <InputLabel
-        htmlFor="custom-css-standard-input"
-        classes={{
-          root: classes.cssLabel,
-          focused: classes.cssFocused,
-        }}
-      >
-        LastKM
-      </InputLabel>
-      <Input
-        id="custom-css-standard-input"
-        classes={{
-          underline: classes.cssUnderline,
-        }}
-      />
-      <InputLabel
-      htmlFor="custom-css-standard-input"
-      classes={{
-        root: classes.cssLabel,
-        focused: classes.cssFocused,
-      }}
-    >
-      Total trip
-    </InputLabel>
-    <Input
-      id="custom-css-standard-input"
-      classes={{
-        underline: classes.cssUnderline,
-      }}
-    />
-    <InputLabel
-    htmlFor="custom-css-standard-input"
-    classes={{
-      root: classes.cssLabel,
-      focused: classes.cssFocused,
-    }}
-  >
-    Driver Name
-  </InputLabel>
-  <Input
-    id="custom-css-standard-input"
-    classes={{
-      underline: classes.cssUnderline,
-    }}
-  />
-  <InputLabel
-  htmlFor="custom-css-standard-input"
-  classes={{
-    root: classes.cssLabel,
-    focused: classes.cssFocused,
-  }}
->
-  Note 
-</InputLabel>
-<Input
-  id="custom-css-standard-input"
-  classes={{
-    underline: classes.cssUnderline,
-  }}
-/>
+          <div className={classes.startEnd}>
+            <TextField
+              className={classes.narrow}
+              id="text1"
+              autoFocus={true}
+              type="text"
+              name="startKM"
+              label="Start KM"
+              margin="normal"
+              color="secondary"
+              value={this.props.startKM}
+              onChange={this.props.handleChange("startKM")}
+            />
+
+            <TextField
+              className={classes.narrow}
+              id="text2"
+              autoFocus={true}
+              type="text"
+              name="endKM"
+              label="End Km"
+              margin="normal"
+              value={this.props.endKM}
+              onChange={this.props.handleChange("endKM")}
+            />
+          </div>
+          <TextField
+            className={classes.normal}
+            id="text3"
+            autoFocus={true}
+            name="total"
+            type="text"
+            label="Total"
+            value={this.props.total}
+            onChange={this.props.handleChange("total")}
+          />
+          <TextField
+            className={classes.normal}
+            id="text4"
+            autoFocus={true}
+            type="text"
+            label="Driver"
+            name="driver"
+            value={this.props.driver}
+            onChange={this.props.handleChange("driver")}
+          />
+          <TextField
+            className={classes.normal}
+            id="text5"
+            autoFocus={true}
+            type="text"
+            label="Note"
+            name="notes"
+            value={this.props.notes}
+            onChange={this.props.handleChange("notes")}
+          />
         </FormControl>
       </div>
     );
@@ -197,7 +153,7 @@ class RadioButtonsGroup extends React.Component {
 }
 
 RadioButtonsGroup.propTypes = {
-  classes: PropTypes.object.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(RadioButtonsGroup);
