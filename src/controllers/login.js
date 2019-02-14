54383f98076e63a3bfbe870ddaa7bcb2cc8faaa0
orjwan98 <orjwan98@hotmail.com>
2019-02-14 09:05:58 +0200

    add cookie and hash it

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,16 +1,27 @@
 const bcrypt = require('bcryptjs');
 const Sequelize = require('sequelize');
+const jwt = require('jsonwebtoken');
 require('env2')('.env');
 
 exports.post = (req, res) => {
   const { username, password } = req.body;
   const hashed = process.env.password;
   const userhere = process.env.username;
+  const secret = process.env.SECRET;
   if (userhere === username) {
     bcrypt.compare(password, hashed, (err, match) => {
       if (err) {
         res.send('There was a problem HERE');
       } else {
+        const token = jwt.sign(
+          {
+            username,
+          },
+          secret,
+        );
+        res.cookie('logged_in', token, {
+          maxAge: 99999,
+        }),
         res.send('Success!');
       }
     });
