056f6e714b8e4497e844e4738f6b32f77da83604
samaamro20 <samaamro20@gmail.com>
2019-02-17 08:54:03 +0200

    fix cars view error

--- src/controllers/cars.js
+++ src/controllers/cars.js
@@ -2,7 +2,7 @@ const { car } = require('../database/models');
 
 exports.get = (req, res) => {
   car.findAll({
-    attributes: ['id', 'model', 'car_no', 'owner', 'last_log_km'],
+    attributes: ['id', 'model_color', 'car_no', 'owner', 'last_log_km'],
     raw: true,
   })
     .then((result) => {
