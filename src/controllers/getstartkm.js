47e57471d06f111fb9d4d74ee0fff3091fb2ef6d
Sama Amro <35188117+Samaamro20@users.noreply.github.com>
2019-02-18 13:39:20 +0200

    Update getstartkm.js

--- src/controllers/getstartkm.js
+++ src/controllers/getstartkm.js
@@ -12,17 +12,3 @@ exports.get = (req, res) => {
     })
     .catch(() => (res.status(500).end()));
 };
-
-//
-// const { car } = require('../database/models');
-//
-// exports.get = (req, res) => {
-//   car.findAll({
-//     attributes: ['id', 'model_color', 'car_no', 'owner', 'last_log_km'],
-//     raw: true,
-//   })
-//     .then((result) => {
-//       res.json(result);
-//     })
-//     .catch(() => (res.status(500).end()));
-// };
