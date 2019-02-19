0898e29e1af3c4a1747c25ab37c1a25a0fa351bd
shaima96 <141062@ppu.edu.ps>
2019-02-19 10:53:20 -0600

    make checkauth a handler instead of a cookie checker

--- src/controllers/checkauth.js
+++ src/controllers/checkauth.js
@@ -1,17 +1,1 @@
-const jwt = require('jsonwebtoken');
-
-const { SECRET, username } = process.env;
-exports.get = (req, res) => {
-  if (req.headers) {
-    const token = req.cookies.logged_in;
-    jwt.verify(token, SECRET, (err, decoded) => {
-      if (err) {
-        return res.status(400).json({ auth: 'false' });
-      }
-      if (decoded.username === username) {
-        return res.status(200).json({ auth: 'true' });
-      }
-      return res.status(400).json({ auth: 'false' });
-    });
-  }
-};
+exports.get = (req, res) => res.json({ auth: true });
