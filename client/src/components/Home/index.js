4e9757ace5339fe818f1969341e1bea4fea84257
orjwan98 <orjwan98@hotmail.com>
2019-02-20 11:21:22 +0200

    render and style the add and show buttons

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,20 +1,57 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
 import Plus from "./Plus.png";
 import Show from "./Show.png";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
 
-const Home = () => {
+const styles = theme => ({
+  icon: {
+    width: "100px",
+    height: "100px",
+    alignSelf: "center"
+  },
+  text: {
+    fontSize: "20px",
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
+    marginTop: "10px"
+  },
+  button: {
+    width: "70%",
+    margin: "7.5% 15% 7.5% 15%",
+    height: "30vh",
+    backgroundColor: "#A11010"
+  },
+
+  container: {
+    display: "flex",
+    flexFlow: "column wrap",
+    justifyContent: "center"
+  }
+});
+
+const Home = ({ classes }) => {
   return (
-    (
-      <Button variant="contained" color="Secondary">
-        <img className="logo" alt="logo" src={Plus} />
+    <React.Fragment>
+      <Button variant="contained" className={classes.button}>
+        <div className={classes.container}>
+          <img alt="logo" src={Plus} className={classes.icon} />
+          <span className={classes.text}>Add Trip</span>
+        </div>
       </Button>
-    ),
-    (
-      <Button variant="contained" color="Secondary">
-        <img className="logos" alt="logo" src={Show} />
+
+      <Button variant="contained" className={classes.button}>
+        <div className={classes.container}>
+          <img alt="logo" src={Show} className={classes.icon} />
+          <span className={classes.text}>Show Reports</span>
+        </div>
       </Button>
-    )
+    </React.Fragment>
   );
 };
-export default Home;
+Home.propTypes = {
+  classes: PropTypes.object.isRequired
+};
+export default withStyles(styles)(Home);
